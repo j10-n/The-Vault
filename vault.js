f32d98da0602b5620338cb6b32c404e8184af453
Justen Nakamoto <justenkn@hawaii.edu>
2019-01-05 15:59:48 -1000

    All Passing

--- vault.js
+++ vault.js
@@ -1,4 +1,27 @@
 'use strict';
 module.exports = function() {
-  
+//     let obj = {};
+
+//     function setValue(key, value){
+//         obj[key] = value
+//     }
+
+//     function getValue(key){
+//         if(obj[key] === undefined){
+//             return null;
+//         }
+//         else{
+//             return obj[key];
+//         }
+//     }
+//   return {
+//       setValue,
+//       getValue
+//     }
+
+    const vault = {};
+    return {
+        getValue: key => (vault.hasOwnProperty(key) ? vault[key] : null),
+        setValue: (key,value) => (vault[key] = value)
+    };
 };
